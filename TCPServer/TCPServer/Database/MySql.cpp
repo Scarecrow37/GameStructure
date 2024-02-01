10a74397a5499349e96d5675c5c3ccc5ed4caf47
Suha Do <weqa123@naver.com>
2024-02-01 15:57:58 +0900

    Import MySql library. Create MySql Class. Create Connect function.

---
+++ TCPServer/TCPServer/Database/MySql.cpp
@@ -0,0 +1,27 @@
+﻿#include "MySql.h"
+
+#pragma comment(lib, "debug/mysqlcppconn")
+
+MySql::MySql() : _connection(nullptr), _connected(false)
+{
+    _driver = get_driver_instance();
+}
+
+void MySql::Connect(const char* hostName, const char* userName, const char* password)
+{
+    _connection = _driver->connect(hostName, userName, password);
+    if (_connection == nullptr)
+    {
+        _connected = false;
+        throw GetException("Connection fail.");
+    }
+    else
+    {
+        _connected = true;
+    }
+}
+
+std::exception MySql::GetException(const char* message)
+{
+    return std::exception(message);
+}
---
+++ TCPServer/TCPServer/Database/MySql.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+#ifndef __MYSQL_H__
+#define __MYSQL_H__
+
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/connection.h>
+
+
+class MySql
+{
+public:
+    MySql();
+
+    void Connect(const char* hostName, const char* userName, const char* password);
+
+protected:
+    static std::exception GetException(const char* message);
+
+private:
+    sql::Driver* _driver;
+    sql::Connection* _connection;
+
+    bool _connected;
+};
+#endif
